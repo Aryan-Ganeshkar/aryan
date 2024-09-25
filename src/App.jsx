b55c3a3a2eca8c7b26b3ae0533dfdd80269c7a34
Aryangit@9284 <ganeshkararyan@gmail.com>
2024-09-25 14:31:32 +0530

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl bg-red-500 text-center font-bold underline">
+      <h1 className="text-3xl bg-blue-500 text-center font-bold underline">
       Hello Aryan
     </h1>
     </>
